316955db123073c2fe108ed8e164dc42a417ea02
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-04-13 17:22:35 +0200

    adding user Entity

---
+++ user-service/src/main/java/org/microarchitecturovisco/userservice/domain/User.java
@@ -0,0 +1,33 @@
+package org.microarchitecturovisco.userservice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String surname;
+
+    public User() {
+    }
+}
