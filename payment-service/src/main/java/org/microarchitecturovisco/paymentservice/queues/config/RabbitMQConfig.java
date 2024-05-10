7c53736248501c076cea162ae7d63f2582da1045
CzajaLukasz <czajalukasz5@gmail.com>
2024-05-10 16:23:56 +0200

    RSWW-29 added rabbitMQ dependencies and config files

--- payment-service/src/main/java/org/microarchitecturovisco/paymentservice/queues/config/RabbitMQConfig.java
+++ payment-service/src/main/java/org/microarchitecturovisco/paymentservice/queues/config/RabbitMQConfig.java
@@ -1,4 +1,17 @@
 package org.microarchitecturovisco.paymentservice.queues.config;
 
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class RabbitMQConfig {
+    @Bean
+    public SimpleMessageConverter converter() {
+        SimpleMessageConverter converter = new SimpleMessageConverter();
+        converter.setAllowedListPatterns(List.of("org.microarchitecturovisco.*", "java.*"));
+        return converter;
+    }
 }
