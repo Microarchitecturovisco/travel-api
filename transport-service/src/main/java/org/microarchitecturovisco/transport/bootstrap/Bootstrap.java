a8db2cff59d856a43701cb9c82ddf942606c64d4
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-05-12 11:58:08 +0200

    Update Bootstrap.java

--- transport-service/src/main/java/org/microarchitecturovisco/transport/bootstrap/Bootstrap.java
+++ transport-service/src/main/java/org/microarchitecturovisco/transport/bootstrap/Bootstrap.java
@@ -5,9 +5,19 @@ import org.microarchitecturovisco.transport.bootstrap.util.parsers.TransportRese
 import org.microarchitecturovisco.transport.bootstrap.util.parsers.LocationParser;
 import org.microarchitecturovisco.transport.bootstrap.util.parsers.TransportCoursesParser;
 import org.microarchitecturovisco.transport.bootstrap.util.parsers.TransportParser;
+import org.microarchitecturovisco.transport.model.dto.TransportDto;
+import org.microarchitecturovisco.transport.model.dto.request.GetTransportsBySearchQueryRequestDto;
+import org.microarchitecturovisco.transport.model.dto.response.GetTransportsBySearchQueryResponseDto;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
 
 
 @Component
@@ -20,10 +30,10 @@ public class Bootstrap implements CommandLineRunner {
     private final LocationParser locationParser;
     private final TransportParser transportParser;
     private final TransportCoursesParser transportCoursesParser;
-    private final String dataDirectory = "transport-service\\src\\main\\java\\org\\microarchitecturovisco\\transport\\bootstrap\\data\\";
 
     @Override
     public void run(String... args) throws Exception {
+        String dataDirectory = "transport-service\\src\\main\\java\\org\\microarchitecturovisco\\transport\\bootstrap\\data\\";
         String hotelCsvFile = dataDirectory + "hotels.csv";
         String hotelDepartureOptionsCsvFile = dataDirectory + "hotel_departure_options.csv";
         String transportsSampleCsvFile = dataDirectory + "transports_sample.csv";
@@ -31,6 +41,13 @@ public class Bootstrap implements CommandLineRunner {
 
         locationParser.importLocationsAbroad(hotelCsvFile);
         locationParser.importLocationsPoland(hotelDepartureOptionsCsvFile);
+
+        transportCoursesParser.createTransportCourses(hotelCsvFile, hotelDepartureOptionsCsvFile);
+
+        transportParser.importTransports(transportsSampleCsvFile);
+
+        transportReservationParser.importTransportReservations(transportReservationSampleCsvFile);
+    }
 
     @Scheduled(fixedDelay = 10000)
     public void testGetTransportsBySearchQuery() {
@@ -46,44 +63,21 @@ public class Bootstrap implements CommandLineRunner {
                 .childrenUnderTen(1)
                 .childrenUnderEighteen(1)
                 .build();
-        transportCoursesParser.createTransportCourses(hotelCsvFile, hotelDepartureOptionsCsvFile);
 
         rabbitTemplate.convertAndSend("transports.requests.getTransportsBySearchQuery", testRequestDto);
-        transportParser.importTransports(transportsSampleCsvFile);
+    }
 
     @RabbitListener(queues = "transports.responses.getTransportsBySearchQuery")
     @RabbitHandler
     public void consumeGetTransportsResponse(GetTransportsBySearchQueryResponseDto responseDto) {
 
-    private void createPlaneConnections(Logger logger, List<TransportCourse> planeCourses, Map<Integer, List<String>> departureCitiesMap, Map<Integer, Location> hotelLocationMap) {
-        for (Map.Entry<Integer, List<String>> entry : departureCitiesMap.entrySet()) {
-            int hotelId = entry.getKey();
-            List<String> departureCities = entry.getValue();
-
-            // Check if hotelId exists in hotel location map
-            if (hotelLocationMap.containsKey(hotelId)) {
-                Location hotelLocation = hotelLocationMap.get(hotelId);
-                String arrivalCountry = hotelLocation.getCountry();
-                String arrivalRegion = hotelLocation.getRegion();
-                Location planeArrivalLocation = locationRepository.findByCountryAndRegion(arrivalCountry, arrivalRegion);
-
-                // Iterate through each departure city for the current hotel
-                for (String departureCity : departureCities) {
-                    String departureCountry = "Polska";
-                    String departureRegion = departureCity;
-                    Location departureLocation = locationRepository.findByCountryAndRegion(departureCountry, departureRegion);
-
-                    // Create transport courses
-                    planeCourses.add(transportCourseRepository.save(TransportCourse.builder()
-                            .departureFrom(departureLocation)
-                            .arrivalAt(planeArrivalLocation)
-                            .type(TransportType.PLANE).build()));
-                    planeCourses.add(transportCourseRepository.save(TransportCourse.builder()
-                            .arrivalAt(departureLocation)
-                            .type(TransportType.PLANE).build()));
-
+        System.out.println("Received transports:");
+        for (TransportDto transportDto : responseDto.getTransportDtoList()) {
+            System.out.println("  - " + transportDto.getTransportCourse().getDepartureFromLocation().getRegion() + " <-> " + transportDto.getTransportCourse().getArrivalAtLocation().getRegion());
+            System.out.println("    " + transportDto.getDepartureDate());
+        }
 
         System.out.println("Received transports of size " + responseDto.getTransportDtoList().size());
     }
-
 }
+
