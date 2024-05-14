77a5963ddb1e74d97af7655c214a339f00418adb
Mixss <michalziemiec@wp.pl>
2024-05-14 17:36:11 +0200

    RSWW-67: Reservation Module: fixed rest of things
    Signed-off-by: Mixss <michalziemiec@wp.pl>

--- reservation-service/src/main/java/org/microarchitecturovisco/reservationservice/repositories/ReservationEventStore.java
+++ reservation-service/src/main/java/org/microarchitecturovisco/reservationservice/repositories/ReservationEventStore.java
@@ -1,8 +1,7 @@
 package org.microarchitecturovisco.reservationservice.repositories;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.microarchitecturovisco.reservationservice.domain.events.ReservationEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationEventStore extends JpaRepository<ReservationEvent, Integer> {
+public interface ReservationEventStore extends JpaRepository<ReservationEvent, String> {
 }
