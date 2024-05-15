b79542cbb1114a47f77491e767bebf1b7e5dd608
Paweł Cichowski <pcichowski@pcichowski.com>
2024-05-15 14:15:43 +0200

    RSWW-72 payment-service: Bootstrap: add RPC example
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- payment-service/src/main/java/org/microarchitecturovisco/paymentservice/Bootstrap.java
+++ payment-service/src/main/java/org/microarchitecturovisco/paymentservice/Bootstrap.java
@@ -4,10 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.microarchitecturovisco.paymentservice.models.dto.HandlePaymentRequestDto;
 import org.microarchitecturovisco.paymentservice.utils.JsonConverter;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,29 +15,49 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class Bootstrap implements CommandLineRunner {
 
-    private final RabbitTemplate paymentRabbitTemplate;
     private final RabbitTemplate rabbitTemplate;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
     }
 
-    @Bean
-    public DirectExchange testDirectExchange() {
-        return new DirectExchange("payments.responses.handle");
-    }
+    private final DirectExchange exchange;
 
 
     @Scheduled(fixedRate = 5000, initialDelay = 5000)
     public void testPayment() {
 
-        String response = (String) paymentRabbitTemplate.convertSendAndReceive("payments.requests.handle", "payments.handlePayment", JsonConverter.convertHandlePaymentRequestDto(
+        UUID testId = UUID.randomUUID();
+
+        System.out.println("Agent 1 Sending request with uuid " + testId);
+
+        String response = (String) rabbitTemplate.convertSendAndReceive(exchange.getName(), "payments.handlePayment", JsonConverter.convertHandlePaymentRequestDto(
                 HandlePaymentRequestDto.builder()
-                        .id(UUID.randomUUID())
+                        .id(testId)
                         .cardNumber("1234567812345678")
                         .idReservation(UUID.randomUUID())
                         .build()
         ));
+
+        System.out.println("Agent 1 Received response " + response);
+    }
+
+    @Scheduled(fixedRate = 5000, initialDelay = 10000)
+    public void testPayment2() {
+
+        UUID testId = UUID.randomUUID();
+
+        System.out.println("Agent 2 Sending request with uuid " + testId);
+
+        String response = (String) rabbitTemplate.convertSendAndReceive(exchange.getName(), "payments.handlePayment", JsonConverter.convertHandlePaymentRequestDto(
+                HandlePaymentRequestDto.builder()
+                        .id(testId)
+                        .cardNumber("1234567812345677")
+                        .idReservation(UUID.randomUUID())
+                        .build()
+        ));
+
+        System.out.println("Agent 2 Received response " + response);
     }
 }
