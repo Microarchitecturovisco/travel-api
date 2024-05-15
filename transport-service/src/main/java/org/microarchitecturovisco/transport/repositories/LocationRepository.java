fc10bed6b7af5edf168eaa093c1c1668f7a5f03b
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-05-15 13:53:42 +0200

    RSWW-46-locations and transport courses creation works

--- transport-service/src/main/java/org/microarchitecturovisco/transport/repositories/LocationRepository.java
+++ transport-service/src/main/java/org/microarchitecturovisco/transport/repositories/LocationRepository.java
@@ -4,4 +4,5 @@ import org.microarchitecturovisco.transport.model.domain.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+    Location findByCountryAndRegion(String country, String region);
 }
