d8104d76ee192a77244cb47122cae0f865576291
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-05-15 16:48:37 +0200

    RSWW-47 creating custom room reservations data

--- hotel-service/src/main/java/org/microarchitecturovisco/hotelservice/model/dto/RoomReservationDto.java
+++ hotel-service/src/main/java/org/microarchitecturovisco/hotelservice/model/dto/RoomReservationDto.java
@@ -1,12 +1,13 @@
 package org.microarchitecturovisco.hotelservice.model.dto;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
 import org.microarchitecturovisco.hotelservice.model.domain.Room;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+@Data
+@Builder
 public class RoomReservationDto {
 
     private UUID reservationId;
@@ -15,4 +16,7 @@ public class RoomReservationDto {
 
     private LocalDateTime dateTo;
 
+    private RoomDto room;
+
+    public RoomReservationDto(){}
 }
