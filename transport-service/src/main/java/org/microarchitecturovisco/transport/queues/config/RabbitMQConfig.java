b442f99265a8a05d50bdc02e092094c65e931877
Paweł Cichowski <pcichowski@pcichowski.com>
2024-05-19 22:33:59 +0200

    RSWW-105 transport-service: remove json message converters
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- transport-service/src/main/java/org/microarchitecturovisco/transport/queues/config/RabbitMQConfig.java
+++ transport-service/src/main/java/org/microarchitecturovisco/transport/queues/config/RabbitMQConfig.java
@@ -6,30 +6,19 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class RabbitMQConfig {
     @Bean
-    public SimpleMessageListenerContainer listenerContainer(ConnectionFactory connectionFactory,
-                                                            MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean(name="jsonMessageConverter")
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public MessageListenerAdapter listenerAdapter(TransportsQueryController consumer, @Qualifier("jsonMessageConverter") MessageConverter messageConverter) {
-        MessageListenerAdapter adapter = new MessageListenerAdapter(consumer, "consumeMessageFromQueue");
-        adapter.setMessageConverter(messageConverter);
-        return adapter;
+    public SimpleMessageConverter converter() {
+        SimpleMessageConverter converter = new SimpleMessageConverter();
+        converter.setAllowedListPatterns(List.of("org.microarchitecturovisco.*", "java.*"));
+        return converter;
     }
 }
