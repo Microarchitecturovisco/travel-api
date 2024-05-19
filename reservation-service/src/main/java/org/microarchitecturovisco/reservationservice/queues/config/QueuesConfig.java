eaed9439bcd6aa2b00fb8baf08ac20a777a0e0b7
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-05-19 13:39:45 +0200

    RSWW-85 Fix compilation issues

--- reservation-service/src/main/java/org/microarchitecturovisco/reservationservice/queues/config/QueuesConfig.java
+++ reservation-service/src/main/java/org/microarchitecturovisco/reservationservice/queues/config/QueuesConfig.java
@@ -1,7 +1,5 @@
 package org.microarchitecturovisco.reservationservice.queues.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,12 +9,4 @@ public class QueuesConfig {
     public static final String QUEUE_TRANSPORT_CHECK_AVAILABILITY_REQ = "transports.requests.checkAvailabilityByQuery.queue";
     public static final String ROUTING_KEY_TRANSPORT_CHECK_AVAILABILITY_REQ = "transports.requests.checkAvailabilityByQuery.routingKey";
 
-    @Bean
-    public Queue handleReservationQueue() {
-        return new Queue(QUEUE_HOTEL_BOOK_REQ);
-    }
-    @Bean
-    public Binding handleReservationRequestBinding(TopicExchange handleReservationExchange, Queue handleReservationQueue) {
-        return BindingBuilder.bind(handleReservationQueue).to(handleReservationExchange).with(ROUTING_KEY_HOTEL_BOOK_REQ);
-    }
 }
