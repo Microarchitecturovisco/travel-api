c2ef407a66b5ce6c9af43a5fb91263292435276e
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-06-09 14:13:15 +0200

    RSWW-140 check if reservation was paid or not

--- reservation-service/src/main/java/org/microarchitecturovisco/reservationservice/services/saga/InvalidPaymentHandler.java
+++ reservation-service/src/main/java/org/microarchitecturovisco/reservationservice/services/saga/InvalidPaymentHandler.java
@@ -5,16 +5,20 @@ import org.microarchitecturovisco.reservationservice.domain.commands.DeleteReser
 import org.microarchitecturovisco.reservationservice.domain.dto.requests.HotelReservationDeleteRequest;
 import org.microarchitecturovisco.reservationservice.domain.dto.requests.ReservationRequest;
 import org.microarchitecturovisco.reservationservice.domain.dto.requests.TransportReservationDeleteRequest;
+import org.microarchitecturovisco.reservationservice.domain.entity.Reservation;
+import org.microarchitecturovisco.reservationservice.repositories.ReservationRepository;
 import org.microarchitecturovisco.reservationservice.services.ReservationAggregate;
 import org.microarchitecturovisco.reservationservice.services.ReservationService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +26,9 @@ public class InvalidPaymentHandler {
     private final BookHotelsSaga bookHotelsSaga;
     private final BookTransportsSaga bookTransportsSaga;
     private final ReservationAggregate reservationAggregate;
-
+    private final ReservationRepository reservationRepository;
     public static final int PAYMENT_TIMEOUT_SECONDS = 60;
+    public static Logger logger = Logger.getLogger(InvalidPaymentHandler.class.getName());
 
     public void schedulePaymentTimeoutCheck(ReservationRequest reservationRequest){
         Runnable paymentTimeoutRunnable = () -> {
@@ -34,9 +39,18 @@ public class InvalidPaymentHandler {
     }
 
     private void paymentTimeout(ReservationRequest reservationRequest) {
-        ReservationService.logger.warning("PAYMENT TIMEOUT FOR ID: " + reservationRequest.getId() + " !");
+        Optional<Reservation> reservationOptional = reservationRepository.findById(reservationRequest.getId());
 
-        rollbackReservation(reservationRequest);
+        if(reservationOptional.isPresent()){
+            Reservation reservation = reservationOptional.get();
+            if(!reservation.isPaid()){
+                ReservationService.logger.warning("PAYMENT TIMEOUT FOR ID: " + reservationRequest.getId() + " !");
+                rollbackReservation(reservationRequest);
+            }
+            else {
+                ReservationService.logger.warning("Reservation with ID: " + reservationRequest.getId() + " was registered as 'paid successfully'");
+            }
+        }
     }
 
     public void rollbackReservation(ReservationRequest reservationRequest){
@@ -106,5 +120,4 @@ public class InvalidPaymentHandler {
                 .build();
         reservationAggregate.handleDeleteReservationCommand(command);
     }
-
 }
